3e4b966dad4fef1c2c7906865df27b948507f7cf
Ana Belen Chamba Mera <venom_anne@hotmail.com>
2020-10-20 17:18:42 -0500

    Users

--- In-Home/Areas/Users/Pages/Account/Register.cshtml.cs
+++ In-Home/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -3,16 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using In_Home.Areas.Users.Models;
+using In_Home.Library;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace In_Home.Areas.Users.Pages.Account
 {
     public class RegisterModel : PageModel
     {
+        private SignInManager<IdentityUser> _signInManager;
+        private UserManager<IdentityUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+        private LUsersRoles _usersRole;
+        public RegisterModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _usersRole = new LUsersRoles();
+        }
         public void OnGet()
         {
+            Input = new InputModel
+            {
+                rolesLista = _usersRole.getRoles(_roleManager) 
+            };
         }
         [BindProperty]
         public InputModel Input { get; set; }
@@ -21,6 +42,7 @@ namespace In_Home.Areas.Users.Pages.Account
             public IFormFile AvatarImage { get; set; }
             [TempData]
             public string ErrorMessage { get; set; }
+            public List<SelectListItem> rolesLista { get; set; }
         }
     }
 }
