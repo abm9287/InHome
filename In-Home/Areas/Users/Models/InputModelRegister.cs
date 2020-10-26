913d8b7c35ac570fa5a51b889a1fcfb6a139ddca
Ana Belen Chamba Mera <venom_anne@hotmail.com>
2020-10-26 14:24:26 -0500

    Información del Usuario.

--- In-Home/Areas/Users/Models/InputModelRegister.cs
+++ In-Home/Areas/Users/Models/InputModelRegister.cs
@@ -9,23 +9,31 @@ namespace In_Home.Areas.Users.Models
 {
     public class InputModelRegister
     {
+        //Campo nombre
         [Required(ErrorMessage = "The Name field is required.")]
         public string Name { get; set; }
+        //Campo apellido
         [Required(ErrorMessage = "Surname field is requiredo")]
         public string LastName { get; set; }
+        //Campo CI
         [Required(ErrorMessage = "The Identity Card field is required")]
         public string CI { get; set; }
+        //Campo  Número de teléfono
         [Required(ErrorMessage = "The Phone Number field is required ")]
         [DataType(DataType.PhoneNumber)]
         [RegularExpression(@"^\(?([0-9]{2})\)?[-. ]?([0-9]{2})[-. ]?([0-9]{5})$", ErrorMessage = "The phone format entered is not valid.")]
         public string PhoneNumber { get; set; }
+        //Campo Email
         [Required(ErrorMessage = "Email field is required")]
+        [EmailAddress(ErrorMessage = "Email is not a valid email addressa.")]
         public string Email { get; set; }
+        //Campo contraseña
         [Display(Name = "Password")]
         [Required(ErrorMessage = "Password field is required.")]
         [StringLength(100, ErrorMessage = "The number of characters in {0} must be at least {2}.", MinimumLength = 8)]
         public string Password { get; set; }
-        [Required(ErrorMessage = "Password field is required")]
+       
+        //Campo rol
         public string Role { get; set; }
 
         //Propiedades
---
+++ In-Home/Areas/Users/Pages/Account/Details.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using In_Home.Areas.Users.Models;
+using In_Home.Data;
+using In_Home.Library;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace In_Home.Areas.Users.Pages.Account
+{
+    public class DetailsModel : PageModel
+    {
+        private SignInManager<IdentityUser> _signInManager;
+        private LUser _user;
+        public DetailsModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            RoleManager<IdentityRole> roleManager,
+            ApplicationDbContext context)
+        {
+            _signInManager = signInManager;
+            _user = new LUser(userManager, signInManager, roleManager, context);
+        }
+        public void OnGet(int id)
+        {
+            var data = _user.getTUsuariosAsync(null, id); 
+            if(0<data.Result.Count)
+            {
+                Input = new InputModel
+                {
+                    DataUser = data.Result.ToList().Last(),
+                };
+            }
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public class InputModel
+        {
+            public InputModelRegister DataUser { get; set; }
+        }
+    }
+}
